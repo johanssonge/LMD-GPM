961399b41ffa60e626b6b45e7291d913144fe76a
Erik Johansson <erik.johansson@lmd.ipsl.fr>
2021-05-27 15:55:28 +0200

    Initiate openGPM.py

---
+++ openGPM.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Created on 2021-05-27
+
+Copyright (c) 2021 Erik Johansson
+
+Author(s):
+Erik Johansson <erik.johansson@lmd.ipsl.fr>
+'''
+
+import numpy as np
+import pdb
+if __name__ == '__main__':
+    mainDir = '/scratch/erikj/'
+    mainDataDir = '%s/Data/GPM' %mainDir
+    
+    
+    
+    
+    
+    
+    
+    pdb.set_trace()
+    
+    
+    
+    
+    
